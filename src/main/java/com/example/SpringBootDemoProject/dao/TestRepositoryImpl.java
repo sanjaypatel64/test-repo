cc25b83acee3858c05195cd3b85807362b1f0ad3
Sanjay Patel <sanpatel8@publicisgroupe.net>
2023-11-16 18:12:38 +0530

    updated the responce message

--- src/main/java/com/example/SpringBootDemoProject/dao/TestRepositoryImpl.java
+++ src/main/java/com/example/SpringBootDemoProject/dao/TestRepositoryImpl.java
@@ -3,6 +3,6 @@ package com.example.SpringBootDemoProject.dao;
 public class TestRepositoryImpl implements TestRepository{
     @Override
     public String getStatementDetails(int arrangementID) {
-        return "Custom Message from TestRepositoryImpl with arrangement ID "+arrangementID;
+        return "Message from TestRepositoryImpl with arrangement ID "+arrangementID;
     }
 }
