1b9266197f71655e7ac2a20ec0378d6b68dfd689
Sanjay Patel <sanpatel8@publicisgroupe.net>
2023-11-17 10:51:02 +0530

    added Unit test for service

--- src/test/java/com/example/SpringBootDemoProject/SpringBootDemoProjectApplicationTests.java
+++ src/test/java/com/example/SpringBootDemoProject/SpringBootDemoProjectApplicationTests.java
@@ -1,10 +1,33 @@
 package com.example.SpringBootDemoProject;
 
+import com.example.SpringBootDemoProject.dao.TestRepository;
+import com.example.SpringBootDemoProject.service.TestService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBootDemoProjectApplicationTests {
+
+	@MockBean
+	private TestService testService;
+
+	@MockBean
+	private TestRepository testRepository;
+
+	@Test
+	public void getStatementDetails() {
+		int arrangementID = 2;
+		when(testRepository.getStatementDetails(arrangementID)).thenReturn("Test String");
+		Assert.assertNull(testService.getStatementDetails(arrangementID));
+	}
 
 	@Test
 	void contextLoads() {
