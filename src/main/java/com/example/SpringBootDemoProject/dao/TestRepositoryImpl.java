a2950d9ac14cc354262b4bf85d4e69c0fa05f66b
sanjaypatel64 <150211143+sanjaypatel64@users.noreply.github.com>
2023-11-17 11:01:33 +0530

    Eng-21347 Added Junit test for service. (#3)
    * updated the responce message
    
    * added Unit test for service
    
    ---------
    
    Co-authored-by: Sanjay Patel <sanpatel8@publicisgroupe.net>

--- src/main/java/com/example/SpringBootDemoProject/dao/TestRepositoryImpl.java
+++ src/main/java/com/example/SpringBootDemoProject/dao/TestRepositoryImpl.java
@@ -1,8 +1,21 @@
 package com.example.SpringBootDemoProject.dao;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 public class TestRepositoryImpl implements TestRepository{
+
     @Override
     public String getStatementDetails(int arrangementID) {
-        return "Message from TestRepositoryImpl with arrangement ID "+arrangementID;
+
+        String result = TestMessage(arrangementID);
+
+        System.out.println(result);
+        return result;
+
     }
+
+    private String TestMessage(int arrangementID){
+        return "Test String";
+    }
+
 }
